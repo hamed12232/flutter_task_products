6703c9626e0d61d4c0f620c1cab0bd21d43856ba
hamed12232 <hamedelshafeei41@gmail.com>
2025-09-16 16:21:24 +0300

    add ScreenUtilInit

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
 class AppColors {
-  static const Color primary = Color(0xFF000000);
+  static const Color primary = Color(0xFFfefeff);
   static const Color borderProductColor = Color(0xFF86a1b3);
   static const Color titleProductColor = Color(0xFF292760);
   static const Color discountProductColor = Color(0xFF4a8eba);
--- lib/view/product_list_screen.dart
+++ lib/view/product_list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_task_products/core/utils/app_colors.dart';
 
 class ProductListScreen extends StatefulWidget {
   const ProductListScreen({super.key});
@@ -11,6 +12,7 @@ class _ProductListScreenState extends State<ProductListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.primary,
       body: ListView.builder(itemBuilder: (context, index) {
         return Container(
 
