0e85779dbad972e309ebcd60debab8cd6eddea1d
hamed12232 <hamedelshafeei41@gmail.com>
2025-09-16 16:30:17 +0300

    add AppWidth and AppHight

---
+++ lib/core/utils/app_hight.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppHight {
+  static double h100 = 100.h;
+
+}
--- lib/view/product_list_screen.dart
+++ lib/view/product_list_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_task_products/core/utils/app_colors.dart';
+import 'package:flutter_task_products/core/utils/app_hight.dart';
+import 'package:flutter_task_products/core/utils/app_width.dart';
 
 class ProductListScreen extends StatefulWidget {
   const ProductListScreen({super.key});
@@ -15,6 +17,13 @@ class _ProductListScreenState extends State<ProductListScreen> {
       backgroundColor: AppColors.primary,
       body: ListView.builder(itemBuilder: (context, index) {
         return Container(
+          width: AppWidth.w100,
+          height: AppHight.h100,
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: AppColors.borderProductColor
+            )
+          ),
 
         );
       },),
